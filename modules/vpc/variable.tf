170b6862f3dc5651c113a67347777029d20697a5
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-21 23:00:18 +0530

    1.create VPC 2.create subnets 3.peer between two VPC's 4.create routes in route table 5.create an instance with spot

---
+++ modules/vpc/variable.tf
@@ -0,0 +1,6 @@
+variable "vpc_cidr_block" {}
+variable "env" {}
+variable "subnet_cidr_block" {}
+variable "default_vpc_id"{}
+variable "route_target_cidr_block"{}
+variable "default_route_table_id"{}
