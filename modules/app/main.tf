2c014ae95b6ae9c1713f0020cd517f38601a1967
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 21:58:50 +0530

    add subnet to resource

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -20,8 +20,8 @@ resource "aws_security_group" "security_group" {
   name        = "${var.env}-sg"
   vpc_id      = var.vpc_id
   ingress {
-    from_port        = 0
-    to_port          = 0
+    from_port        = 22
+    to_port          = 22
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
 
