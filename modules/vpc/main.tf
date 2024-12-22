7f7480ef3713be3fd7df619257d9b4cfca97304c
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 23:02:51 +0530

    specify ssh protocol port number

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -48,7 +48,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw[count.index].id
+    gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
     Name = "${var.env}-public route-table-${count.index+1}"
