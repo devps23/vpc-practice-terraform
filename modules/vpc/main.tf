ae2a0d8fdee39565c30e5194566df3c7574c7d4e
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 21:52:12 +0530

    add vpc cidr block to default route

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -30,7 +30,7 @@ resource "aws_route" "main_edit_route" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 # Edit routes on default vpc route
-resource "aws_route" "main_edit_route" {
+resource "aws_route" "default_edit_route" {
   route_table_id            = var.default_route_table_id
   destination_cidr_block    = var.vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
