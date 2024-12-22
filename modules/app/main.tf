57644b0f362655122eb1d6b181b671ccc5c638f6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 22:01:13 +0530

    add subnet to resource

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "security_group" {
   ingress {
     from_port        = 22
     to_port          = 22
-    protocol         = "SSH"
+    protocol         = "TCP"
     cidr_blocks      = ["0.0.0.0/0"]
 
   }
