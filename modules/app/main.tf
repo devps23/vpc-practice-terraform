4d50638239714b1901436abb9957ae442440c3a4
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-03 15:41:46 +0530

    nat gateway and eip

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.security_group.id]
-  //subnet_id = var.subnet_id[0]
+  subnet_id = var.subnet_id[0]
   instance_market_options {
     market_type = "spot"
     spot_options {
@@ -81,13 +81,13 @@ resource "aws_lb" "lb" {
 # create a target group
 resource "aws_lb_target_group" "tg" {
   count             = var.lb_needed ? 1 : 0
-  name     = "${var.env}-${var.component}-tg"
-  port     = var.app_port
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name              = "${var.env}-${var.component}-tg"
+  port              = var.app_port
+  protocol          = "HTTP"
+  vpc_id            = var.vpc_id
 }
 # create a target group attachment
-resource "aws_lb_target_group_attachment" "test" {
+resource "aws_lb_target_group_attachment" "tg_attach" {
   count             = var.lb_needed ? 1 : 0
   target_group_arn = aws_lb_target_group.tg[0].arn
   target_id        = aws_instance.instance.id
