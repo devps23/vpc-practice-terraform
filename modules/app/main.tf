59e0c4753af8b878761f202780bef094566617c4
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-04 21:19:26 +0530

    change vault host

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -61,7 +61,7 @@ resource "null_resource" "null_instance" {
   }
 }
 resource "aws_route53_record" "server_record" {
-  count = var.lb_needed ? 0 : 1
+  count = var.lb_needed ? 0 : 1 || var.component == "frontend"
   name      = "${var.env}-${var.component}-dns"
   type      = "A"
   zone_id   = var.zone_id
