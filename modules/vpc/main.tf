0d05253ab0b52f4efb551d58c60b65370224e681
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-04 21:27:20 +0530

    uncomment

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -54,49 +54,49 @@ resource "aws_route_table" "public_route_table" {
     Name = "${var.env}-public route-table-${count.index+1}"
   }
 }
-# create nat gateway
-# resource "aws_nat_gateway" "nat" {
-#   count = length(var.public_subnets)
-#   allocation_id = aws_eip.eip[count.index].id
-#   subnet_id     = aws_subnet.public_subnets[count.index].id
-#   tags = {
-#     Name = "${var.env}-nat-${count.index+1}"
-#   }
-# }
-#   resource "aws_eip" "eip" {
-#     count = length(var.public_subnets)
-#     domain   = "vpc"
-# }
-# create private route table
-# resource "aws_route_table" "frontend_route_table" {
-#   count = length(var.frontend_subnets)
-#   vpc_id = aws_vpc.vpc.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat[count.index].id
-#   }
-#   tags = {
-#     Name = "${var.env}-route-table-${count.index+1}"
-#   }
-# }
+//create nat gateway
+resource "aws_nat_gateway" "nat" {
+  count = length(var.public_subnets)
+  allocation_id = aws_eip.eip[count.index].id
+  subnet_id     = aws_subnet.public_subnets[count.index].id
+  tags = {
+    Name = "${var.env}-nat-${count.index+1}"
+  }
+}
+  resource "aws_eip" "eip" {
+    count = length(var.public_subnets)
+    domain   = "vpc"
+}
+//create private route table
+resource "aws_route_table" "frontend_route_table" {
+  count = length(var.frontend_subnets)
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+  tags = {
+    Name = "${var.env}-route-table-${count.index+1}"
+  }
+}
 # associate route table id to subnet id
 resource "aws_route_table_association" "public-route-association" {
   count = length(var.public_subnets)
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_route_table[count.index].id
 }
-# resource "aws_route_table_association" "frontend-route-association" {
-#   count = length(var.frontend_subnets)
-#   subnet_id      = aws_subnet.frontend_subnets[count.index].id
-#   route_table_id = aws_route_table.frontend_route_table[count.index].id
-# }
+resource "aws_route_table_association" "frontend-route-association" {
+  count = length(var.frontend_subnets)
+  subnet_id      = aws_subnet.frontend_subnets[count.index].id
+  route_table_id = aws_route_table.frontend_route_table[count.index].id
+}
 # edit the route
-# resource "aws_route" "frontend_route" {
-#   count = length(var.frontend_subnets)
-#   route_table_id = aws_route_table.frontend_route_table[count.index].id
-#   destination_cidr_block = var.default_vpc_cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-# }
+resource "aws_route" "frontend_route" {
+  count = length(var.frontend_subnets)
+  route_table_id = aws_route_table.frontend_route_table[count.index].id
+  destination_cidr_block = var.default_vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
 # default edit route
 resource "aws_route" "default_route" {
   count = length(var.frontend_subnets)
