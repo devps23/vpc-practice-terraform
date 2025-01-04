395b09c32db7e6a13035318fb8b43576f1bab2b0
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-04 18:49:45 +0530

    repo changed

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -55,8 +55,8 @@ resource "null_resource" "null_instance" {
     inline = [
       "sudo dnf install ansible",
       "sudo pip3.11 install ansible hvac",
-      "ansible-pull -i localhost. -U https://github.com/pdevpos/learn-ansible get_secrets_vault.yml -e env=${var.env} -e component_name=${var.component} -e vault_token=${var.vault_token}",
-      "ansible-pull -i localhost, -U https://github.com/pdevpos/learn-ansible expense.yml -e env=${var.env} -e component_name=${var.component} -e @secrets.json -e @app.json"
+      "ansible-pull -i localhost. -U https://github.com/devps23/expesne-practice-ansible get_secrets_vault.yml -e env=${var.env} -e component_name=${var.component} -e vault_token=${var.vault_token}",
+      "ansible-pull -i localhost, -U https://github.com/devps23/expense-practice-ansible expense.yml -e env=${var.env} -e component_name=${var.component} -e @secrets.json -e @app.json"
     ]
   }
 }
