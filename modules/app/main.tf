527b0e9a0703032b0012b6bc9daeefd79cc69a8b
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-05 13:31:34 +0530

    added dns name

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -117,7 +117,7 @@ resource "aws_lb_target_group" "tg" {
   port              = var.app_port
   protocol          = "HTTP"
   vpc_id            = var.vpc_id
-
+  deregistration_delay = 15
   health_check {
     healthy_threshold   = 2
     interval            = 5
