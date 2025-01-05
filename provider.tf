1f32ecfec1054c283226571c09230a34be714f05
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-05 13:15:25 +0530

    added dns name

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address        ="https://18.206.180.186:8200"
+  address        ="https://vault-intenral.pdevops72.online:8200"
   token          = var.vault_token
   skip_tls_verify = true
 }
