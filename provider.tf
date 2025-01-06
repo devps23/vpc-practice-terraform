41561df05c2f5e3d3b7a35fa8a0a0930b90776cb
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-06 18:16:50 +0530

    lookup

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
-provider "vault" {
-  address        ="https://vault-internal.pdevops72.online:8200"
-  token          = var.vault_token
-  skip_tls_verify = true
-}
+# provider "vault" {
+#   address        ="https://vault-internal.pdevops72.online:8200"
+#   token          = var.vault_token
+#   skip_tls_verify = true
+# }
